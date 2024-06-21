8dc2de5801f74399d9d7aa26749cebd25d296a40
Yi Liu <gordenfl@berkeley.edu>
2024-06-21 15:38:35 -0700

    finish Q1: Finding a Fixed Food Dot using Depth First Search (DFS)

--- proj1/search/search.py
+++ proj1/search/search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -18,6 +18,7 @@ Pacman agents (in searchAgents.py).
 """
 
 import util
+
 
 class SearchProblem:
     """
@@ -68,9 +69,11 @@ def tinyMazeSearch(problem):
     sequence of moves will be incorrect, so only use this for tinyMaze.
     """
     from game import Directions
+
     s = Directions.SOUTH
     w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+    return [s, s, w, s, w, w, s, w]
+
 
 def depthFirstSearch(problem: SearchProblem):
     """
@@ -86,18 +89,58 @@ def depthFirstSearch(problem: SearchProblem):
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    point = problem.getStartState()  #
+    reached = set()
+    stack = []
+    cache = {}
+    reached.add(point)
+    goal = None
+    for i in problem.getSuccessors(point):  # put all the end point into stack at first
+        cache[i] = (point, None, None)
+        stack.append(i)
+
+    while stack:
+        u = stack.pop()
+        if u[0] in reached:  # has already been in u[0] ignore
+            continue
+        if problem.isGoalState(u[0]):  # if this u[0] is target we can break
+            goal = u
+            break
+
+        # else
+        reached.add(u[0])  # u[0] has been visited
+        for i in problem.getSuccessors(
+            u[0]
+        ):  # get next can be reached place push into stack
+            cache[i] = (
+                u  # P is the path dict, we can find the next node's Previous node
+            )
+            stack.append(i)
+
+    # until we meet the target or the stack become empty, every place we have been reached
+    path, current_node = [], goal
+    while current_node != (point, None, None):  # we add all the
+        path.append(
+            current_node[1]
+        )  # From the goal back to get the path, add into path
+        current_node = cache[current_node]
+    path.reverse()  # reverse our path to the normal return value
+    return path
+    # util.raiseNotDefined()
+
 
 def breadthFirstSearch(problem: SearchProblem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
 
+
 def uniformCostSearch(problem: SearchProblem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
+
 
 def nullHeuristic(state, problem=None):
     """
@@ -105,6 +148,7 @@ def nullHeuristic(state, problem=None):
     goal in the provided SearchProblem.  This heuristic is trivial.
     """
     return 0
+
 
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
